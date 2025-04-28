3fb1f9727f1098e94c3010edac217584f2d74663
Lavie Lederman <158300455+lavieled@users.noreply.github.com>
2025-04-28 15:24:41 +0300

    Update smash.c

--- HW1/smash.c
+++ HW1/smash.c
@@ -41,7 +41,44 @@ int main(int argc, char* argv[])
 		}
 		execute_result = executeCommand(cmd);
 		if (execute_result == 0){
-			// command isn't biult-in, handle here:
+			//Not a built-in command, try run external
+			pid_t child_pid = fork();
+			if (child_pid == -1) {
+			    //fork failed
+			    //perror("smash error: fork failed");
+			    freeCMD(cmd);
+			    continue;
+			}
+			else if (child_pid == 0) {
+			    // Child process
+			    setpgrp(); // Detach the child from smash's group
+			    execvp(cmd->cmd, cmd->args);
+			    // If we get here execvp failed:
+			    perrorSmash("external", "cannot find program");
+			    freeCMD(cmd);
+			    exit(1); // Exit with failure
+			}
+			else {
+			    // Parent process
+			    bool bg = (cmd->nargs >= 0) && (strcmp(cmd->args[cmd->nargs], "&") == 0);
+			    if (bg) {
+			        // Background job: Add to jobs list
+			        Job* job = MALLOC_VALIDATED(Job, sizeof(Job));
+			        job->pid = child_pid;
+			        job->cmd = cmd;
+			        addJob(job);
+			    }
+			    else {
+			        // Foreground job: wait for it
+			        fg_pid = child_pid;
+			        fg_cmd = cmd;
+			        waitpid(child_pid, NULL, 0);
+			        fg_pid = -1;
+			        fg_cmd = NULL;
+			        freeCMD(cmd);
+			    }
+			}
+
 		}
 		//initialize buffers for next command
 		_line[0] = '\0';
