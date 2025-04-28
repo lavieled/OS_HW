c8999699e77c9c10ecd6f886a2d5acf9754ca178
Lavie Lederman <158300455+lavieled@users.noreply.github.com>
2025-04-28 16:08:27 +0300

    Update signals.c

--- HW1/signals.c
+++ HW1/signals.c
@@ -1,8 +1,10 @@
 #include "commands.h"
-#include <signals.h>
+#include "signals.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+#include <signal.h>
 
 extern pid_t fg_pid; // The PID of the process running in the foreground
 extern ParsedCommand* fg_cmd; // The command currently running in the foreground
@@ -11,23 +13,23 @@ void ctrl_c_handler(int sig) {
     printf("smash: caught CTRL+C\n");
 
     if (fg_pid > 0 && fg_pid != getpid()) {
-        // If there is a foreground process and it is not smash itself
         if (kill(fg_pid, SIGKILL) == 0) {
             printf("smash: process %d was killed\n", fg_pid);
         } else {
-            perror("smash error: failed to kill foreground process");
+            perror("smash error: kill failed");
         }
     }
-    fg_pid = -1; // Reset: no foreground process anymore
+    fg_pid = -1;
+    fg_cmd = NULL;
 }
 
 void ctrl_z_handler(int sig) {
     printf("smash: caught CTRL+Z\n");
 
     if (fg_pid > 0 && fg_pid != getpid()) {
-        // If there is a foreground process and it is not smash itself
         if (kill(fg_pid, SIGSTOP) == 0) {
             printf("smash: process %d was stopped\n", fg_pid);
+
             // Add the stopped process to the jobs table
             Job* job = MALLOC_VALIDATED(Job, sizeof(Job));
             job->pid = fg_pid;
@@ -36,8 +38,9 @@ void ctrl_z_handler(int sig) {
             job->state = STOPPED;
             addJob(job);
         } else {
-            perror("smash error: failed to stop foreground process");
+            perror("smash error: kill failed");
         }
     }
-    fg_pid = -1; // Reset: no foreground process anymore
+    fg_pid = -1;
+    fg_cmd = NULL;
 }
