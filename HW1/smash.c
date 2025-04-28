43062361ee8aa2170cf181671396ffb5beafd06b
Lavie Lederman <158300455+lavieled@users.noreply.github.com>
2025-04-28 12:28:19 +0300

    Update smash.c

--- HW1/smash.c
+++ HW1/smash.c
@@ -23,15 +23,30 @@ char _line[CMD_LENGTH_MAX];
 int main(int argc, char* argv[])
 {
 	char _cmd[CMD_LENGTH_MAX];
+	ParsedCommand* cmd;
+	int parse_result;
+	int execute_result;
 	while(1) {
 		printf("smash > ");
 		fgets(_line, CMD_LENGTH_MAX, stdin);
 		strcpy(_cmd, _line);
 		//execute command
-
+		cmd = MALLOC_VALIDATED(ParsedCommand, sizeof(ParsedCommand));
+		parse_result = parseCommand(_cmd, cmd);
+		if (parse_result == INVALID_COMMAND){
+			//initialize buffers for next command
+			_line[0] = '\0';
+			_cmd[0] = '\0';
+			continue;
+		}
+		execute_result = executeCommand(cmd);
+		if (execute_result == 0){
+			// command isn't biult-in, handle here:
+		}
 		//initialize buffers for next command
 		_line[0] = '\0';
 		_cmd[0] = '\0';
+		updateJobTable();
 	}
 
 	return 0;
