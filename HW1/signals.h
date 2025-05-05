37836218f56d5d1f328122dfb411c524a199e192
Lavie Lederman <158300455+lavieled@users.noreply.github.com>
2025-05-05 18:50:19 +0300

    Update signals.h

--- HW1/signals.h
+++ HW1/signals.h
@@ -4,11 +4,26 @@
 /*=============================================================================
 * includes, defines, usings
 =============================================================================*/
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>  // pid_t
+#include <unistd.h>     // fork(), execvp(), getpid()
+#include <sys/wait.h>   // waitpid()
 
 /*=============================================================================
 * global functions
 =============================================================================*/
-void ctrl_c_handler(int sig);
-void ctrl_z_handler(int sig);
+
+void sigTermhandler(int signum);
+void sigStophandler(int signum);
+
+/*=============================================================================
+* global variables
+=============================================================================*/
+
+extern pid_t front_pid;
 
 #endif //__SIGNALS_H__
