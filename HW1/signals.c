5a65a617a6cecc61c9eb8bacd34eb3c5fac327f0
Lavie Lederman <158300455+lavieled@users.noreply.github.com>
2025-05-05 18:50:04 +0300

    Update signals.c

--- HW1/signals.c
+++ HW1/signals.c
@@ -1,47 +1,32 @@
 // signals.c
 #include "signals.h"
 #include "commands.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-#include <time.h>
+//handle CTRL+C/Z signals
 
-extern pid_t fg_pid;
-extern ParsedCommand* fg_cmd;
+extern pid_t front_pid;
 
-void ctrl_c_handler(int sig) {
+void sigTermhandler(int signum) {
     printf("smash: caught CTRL+C\n");
 
-    if (fg_pid > 0 && fg_pid != getpid()) {
-        if (kill(fg_pid, SIGKILL) == 0) {
-            printf("smash: process %d was killed\n", fg_pid);
+     if (front_pid > 0 && front_pid != getpid()) {
+        if (kill(front_pid, SIGKILL) == 0) {
+            printf("smash: process %d was killed\n", front_pid);
         } else {
             perror("smash error: kill failed");
         }
-    }
+	}
+	
+    
+ }//CTRL+C function
 
-    fg_pid = -1;
-    fg_cmd = NULL;
-}
-
-void ctrl_z_handler(int sig) {
+ void sigStophandler(int signum) {
     printf("smash: caught CTRL+Z\n");
-
-    if (fg_pid > 0 && fg_pid != getpid()) {
-        if (kill(fg_pid, SIGSTOP) == 0) {
-            printf("smash: process %d was stopped\n", fg_pid);
-
-            Job* job = MALLOC_VALIDATED(Job, sizeof(Job));
-            job->pid = fg_pid;
-            job->cmd = cloneParsedCommand(fg_cmd); // copy for mem leak?
-            job->start = time(NULL);
-            job->state = STOPPED;
-            addJob(job);
+   if (front_pid > 0 && front_pid != getpid()) {
+        if (kill(front_pid, SIGSTOP) == 0) {
+            printf("smash: process %d was stopped\n", front_pid);
         } else {
-            perror("smash error: kill failed");
+            perror("smash error: stop failed");
         }
     }
+}//CTRL+z function
 
-    fg_pid = -1;
-    fg_cmd = NULL;
-}
